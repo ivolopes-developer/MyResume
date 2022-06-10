b3926bf073e3f82c2617e96a3624c1f4de803504
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2022-06-10 20:04:55 +0100

    Particles on home background
    Particulas na home, feitas com JS.
    
    Apenas na home para o resto da interatividade do site não ficar pesada, principalmente em telemóveis ou monitores com alta resolução.

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,6 +248,73 @@
 
 // INICIO DAS PARTICULAS ----------------------------------------------------------------------------------------------------------------
 
+/* ---- particles.js config ---- */
 
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 380,
+      "density": {
+        "enable": true,
+        "value_area": 3000
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      }
+    },
+    "opacity": {
+      "value": 0,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.5,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": true
+});
 
 // FIM DAS PARTICULAS ----------------------------------------------------------------------------------------------------------------
